2021b9c615345ed36c16fcad12e1fa3d04e60149
Jhony Caranguay <jfernandomainguez@gmail.com~>
2026-04-03 09:58:57 -0500

    otro cambio

--- tuto-vscode/src/proof.py
+++ tuto-vscode/src/proof.py
@@ -1,3 +1,10 @@
-x=4
-y=5
-print(x+y)
+x = 10
+y = 2
+
+resultado = x / y
+
+print(f"Resultado: {resultado}")
+
+for i in range(3):
+    valor = resultado / (i - 1)
+    print(f"Iteración: {i} Valor: {valor}")
